4b8fab963432a8e5a1141631446bbda0ee2189d6
hwanghyeongyu <pipy201@naver.com>
2017-10-27 20:26:41 +0900

    패턴 클래스 분리

--- connector_predict.py
+++ connector_predict.py
@@ -3,22 +3,6 @@ import os
 import predict
 
 
-class Singleton(type):
-    """
-    Define an Instance operation that lets clients access its unique
-    instance.
-    """
-
-    def __init__(cls, name, bases, attrs, **kwargs):
-        super().__init__(name, bases, attrs)
-        cls._instance = None
-
-    def __call__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = super().__call__(*args, **kwargs)
-        return cls._instance
-
-
 class Connector():
     def __init__(self, image_file):
         MODEL_DIR = 'imagenet_model'
